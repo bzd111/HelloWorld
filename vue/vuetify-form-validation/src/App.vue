cae5bb6e7e744aceafb454fbc24ee2e849d9541e
bzd111 <zxc@gogogozxc.xyz>
2020-01-14 10:23:17 +0800

    [Add]: vue and vuetify
    Signed-off-by: bzd111 <zxc@gogogozxc.xyz>

---
+++ vue/vuetify-form-validation/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Form Validation</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-tabs v-model="tab" centered>
+        <v-tab>Login</v-tab>
+        <v-tab>Registration</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+       <v-tab-item name="test1">
+      <LoginForm></LoginForm>
+      </v-tab-item> 
+      <v-tab-item><RegistrationForm></RegistrationForm></v-tab-item>
+      </v-tabs-items> 
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm"
+
+export default {
+  name: "App",
+
+  components: {
+    LoginForm,
+    RegistrationForm
+  },
+
+  data: () => ({
+    tab: null
+  })
+};
+</script>
---
+++ vue/vuetify-form-validation/src/components/RegistrationForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Registration Form
+    </v-card-title>
+    <v-card-text>
+      <v-form v-model="isValid">
+        <v-text-field
+          label="Email"
+          v-model="email"
+          :rules="emailRules"
+          error-count="2"
+          required
+        >
+        </v-text-field>
+        <v-text-field
+          label="password"
+          v-model="password"
+          type="password"
+          :rules="passwordRules"
+          error-count="5"
+          required
+        >
+        </v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" :disabled="!isValid">Register</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "RegistrationForm",
+  data: () => ({
+    email: null,
+    password: null,
+    isValid: true,
+    emailRules: [
+      v => !!v || "Email is required",
+      v => /.+@.+/.test(v) || "Email must be valid"
+    ],
+    passwordRules: [
+      v => !!v || "Password is required",
+      v => (v && v.length >= 5) || "Password must have 5+ characters",
+      v => /(?=.*[A-Z])/.test(v) || "Must have one uppercase character",
+      v => /(?=.*\d)/.test(v) || "Must have one number",
+      v => /([!@$%])/.test(v) || "Must have one special character [!@#$%]"
+    ]
+  })
+};
+</script>
+
+<style></style>
